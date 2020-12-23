bd6901e2ffd6c7769a43969de962f6f72872ce1f
absolutholz <absolutholz@gmail.com>
2020-12-23 17:47:52 +0100

    Update initial size

--- app/src/views/GameView.vue
+++ app/src/views/GameView.vue
@@ -1,13 +1,20 @@
 <template>
 	<main>
 		<game
-			:fieldCount="$route.query.fieldCount * 1 || 64"
-			:mineCount="$route.query.mineCount * 1 || 12"
+			:fieldCount="$route.query.fieldCount * 1 || 81"
+			:mineCount="$route.query.mineCount * 1 || 10"
 		/>
 	</main>
 </template>
 
 <script>
+
+/*
+Easy	|	9x9 	|	10
+Medium	|	16x16	|	39
+Expert	|	30x16	|	95
+*/
+
 import Game from './../components/game/Game';
 
 export default {
