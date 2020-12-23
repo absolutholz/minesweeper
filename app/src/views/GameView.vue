8046c8831cac77946524e042c2a6c5640c4e74d4
absolutholz <absolutholz@gmail.com>
2020-12-23 09:16:18 +0100

    Add game component to game view

--- app/src/views/GameView.vue
+++ app/src/views/GameView.vue
@@ -1,11 +1,20 @@
 <template>
 	<main>
-		Game
+		<game
+			:fieldCount="$route.query.fieldCount * 1 || 64"
+			:mineCount="$route.query.mineCount * 1 || 12"
+		/>
 	</main>
 </template>
 
 <script>
+import Game from './../components/game/Game';
+
 export default {
 	name: 'GameView',
+
+	components: {
+		Game,
+	},
 };
 </script>
