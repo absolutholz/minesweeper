a168b1ae735cdbbc42499435f475edc26072e3d5
absolutholz <absolutholz@gmail.com>
2020-12-29 20:53:46 +0100

    Update indexeddb location and logic and highscore limit

--- app/src/components/game/Game/Game.vue
+++ app/src/components/game/Game/Game.vue
@@ -72,6 +72,7 @@ import SvgRestart from '@mdi/svg/svg/restart.svg';
 import SvgPause from '@mdi/svg/svg/pause.svg';
 
 import Timer from './../../../js/Timer';
+import { createHighScore, readHighScores, deleteHighScore } from './../../../localDb';
 
 export const STATE_GAME_NOT_STARTED = 'game_not_started';
 export const STATE_GAME_PLAYING = 'game_playing';
@@ -134,76 +135,6 @@ export const DIFFICULTIES = [
 		mineRatio: 0.25,
 	},
 ];
-
-const DB_NAME = 'minesweeperHighScores';
-const DB_VERSION = 1;
-const DB_STORE_NAME = 'highScores';
-
-async function getDb () {
-	return new Promise((resolve, reject) => {
-		let request = window.indexedDB.open(DB_NAME, DB_VERSION);
-
-		request.onerror = (event) => {
-			console.log('Error opening db', event);
-			reject('Error');
-		};
-
-		request.onsuccess = (event) => {
-			resolve(event.target.result);
-		};
-
-		request.onupgradeneeded = (event) => {
-			console.log('onupgradeneeded');
-			let db = event.target.result;
-			// let objectStore = db.createObjectStore(DB_STORE_NAME, { autoIncrement: true, keyPath: 'id' });
-			db.createObjectStore(DB_STORE_NAME, { autoIncrement: true, keyPath: 'id' });
-		};
-	});
-}
-
-async function getHighScores (db) {
-	return new Promise((resolve, reject) => {
-		let trans = db.transaction([ DB_STORE_NAME ], 'readonly');
-
-		trans.oncomplete = () => {
-			resolve(highScores);
-		};
-
-		trans.onerror = (event) => {
-			console.log('Error getting high scores', event);
-			reject('Error');
-		};
-
-		let store = trans.objectStore(DB_STORE_NAME);
-		let highScores = [];
-
-		store.openCursor().onsuccess = (event) => {
-			let cursor = event.target.result;
-			if (cursor) {
-				highScores.push(cursor.value);
-				cursor.continue();
-			}
-		};
-	});
-}
-
-async function addHighScore(db, highScore) {
-	return new Promise((resolve, reject) => {
-		let trans = db.transaction([ DB_STORE_NAME ], 'readwrite');
-
-		trans.oncomplete = () => {
-			resolve();
-		};
-
-		trans.onerror = (event) => {
-			console.log('Error adding high score', event);
-			reject('Error');
-		};
-
-		let store = trans.objectStore(DB_STORE_NAME);
-		store.add(highScore);
-	});
-}
 
 export default {
 	name: 'Game',
@@ -298,18 +229,41 @@ export default {
 			this.state = STATE_GAME_LOST;
 		},
 
-		winGame () {
+		async winGame () {
 			this.timer.stop();
 			this.state = STATE_GAME_WON;
-			addHighScore(this.db, {
-				name: 'steve',
-				seconds: this.secondsPlayed,
-				size: this.$route.query.size,
-				difficulty: this.$route.query.difficulty,
-				fields: this.fieldCount,
-				mines: this.mineCount,
-				timestamp: new Date(),
-			});
+
+			const size = this.$route.query.size;
+			const difficulty = this.$route.query.difficulty;
+
+			let highScores = await readHighScores();
+			highScores = highScores
+				.filter((score) => score.size === size && score.difficulty === difficulty)
+				.sort((a, b) => {
+					// https://stackoverflow.com/questions/6913512/how-to-sort-an-array-of-objects-by-multiple-fields/20554416#20554416
+					return a.seconds - b.seconds;
+				});
+			const position = highScores.findIndex((score) => this.secondsPlayed <= score.seconds);
+
+			if (position < 10) {
+				console.log(`made the top 10 @ ${ position + 1 }`);
+				createHighScore({
+					name: 'player unknown',
+					seconds: this.secondsPlayed,
+					size: size,
+					difficulty: difficulty,
+					fields: this.fieldCount,
+					mines: this.mineCount,
+					timestamp: new Date(),
+				});
+
+				if (highScores.length > 9) {
+					highScores.slice(9).forEach((score) => deleteHighScore(score.id));
+				}
+
+			} else {
+				console.log(`not one of your 10 best @ ${ position + 1 }`);
+			}
 		},
 
 		start () {
@@ -359,11 +313,11 @@ export default {
 		this.start();
 	},
 
-	async created() {
-		this.db = await getDb();
-		this.highScores = await getHighScores(this.db);
-		// this.ready = true;
-	},
+	// async created() {
+	// 	// this.db = await getDb();
+	// 	// this.highScores = await getHighScores(this.db);
+	// 	// this.ready = true;
+	// },
 };
 </script>
 
